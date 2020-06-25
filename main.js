0fa8490f7b1b483699c680dc5f69ca953d2f4c17
Tatyana Shurinova <tatyana.shurinova@gmail.com>
2020-06-25 19:49:02 +0300

    finished section events

--- main.js
+++ main.js
@@ -25,3 +25,30 @@ var mySwiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   autoHeight: true,
 });
+
+const onTabClick = function (idContainer, idElement) {
+  let dataAttribute = `[data-${idContainer}`;
+  for (let item of document.querySelectorAll(`${dataAttribute}-name`)) {
+    item.classList.remove("active");
+  }
+  for (let item of document.querySelectorAll(`${dataAttribute}-tab`)) {
+    item.classList.remove("active");
+  }
+  document
+    .querySelector(`${dataAttribute}-name=${idElement}]`)
+    .classList.add("active");
+  document
+    .querySelector(`${dataAttribute}-tab=${idElement}]`)
+    .classList.add("active");
+};
+
+let tabsName = document.querySelectorAll(".tab-name");
+tabsName.forEach((tab) => {
+  tab.addEventListener("click", (event) => {
+    console.log(event.target.dataset.menuTabsName);
+    onTabClick(
+      event.target.parentElement.id,
+      event.target.dataset.menuTabsName
+    );
+  });
+});
